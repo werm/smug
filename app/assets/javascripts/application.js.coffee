7cbeedeec28010653fffa1670533432056e0f5e7
Craig Wermert <craig@wermert.org>
2012-08-03 21:10:34 +0400

    AppDrone: smugsaints

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,13 @@
+//= require jquery
+//= require jquery_ujs
+//= require init
+//= require bootstrap-dropdown
+//= require bootstrap-scrollspy
+//= require bootstrap-tab
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require bootstrap-alert
+//= require bootstrap-button
+//= require bootstrap-collapse
+//= require bootstrap-carousel
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smugsaints::Application.config.secret_token = 'b1cee1c0f280096798ebdaaa7876468f79708dfc21735820702a26496d9e0070323d76156e92899c86586b44859e6d70982d172901cf8f6287012ebf6bac215b'
